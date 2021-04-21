c06421eca594de4e1a6dc7b87567c31720fbe2cd
ericshagrin <ericshagrin@uchicago.edu>
2021-04-21 21:41:36 +0000

    Created resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_163752) do
+ActiveRecord::Schema.define(version: 2021_04_21_165547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2021_04_21_163752) do
     t.index ["sender_id"], name: "index_follow_requests_on_sender_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "fan_id", null: false
+    t.bigint "photo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["fan_id"], name: "index_likes_on_fan_id"
+    t.index ["photo_id"], name: "index_likes_on_photo_id"
+  end
+
   create_table "photos", force: :cascade do |t|
     t.string "image"
     t.integer "comments_count", default: 0
@@ -59,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_163752) do
     t.integer "comments_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "photos_count"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
@@ -68,5 +78,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_163752) do
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "follow_requests", "users", column: "recipient_id"
   add_foreign_key "follow_requests", "users", column: "sender_id"
+  add_foreign_key "likes", "photos"
+  add_foreign_key "likes", "users", column: "fan_id"
   add_foreign_key "photos", "users", column: "owner_id"
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,27 +1,26 @@
 # == Schema Information
 #
-# Table name: comments
+# Table name: likes
 #
 #  id         :bigint           not null, primary key
-#  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  author_id  :bigint           not null
+#  fan_id     :bigint           not null
 #  photo_id   :bigint           not null
 #
 # Indexes
 #
-#  index_comments_on_author_id  (author_id)
-#  index_comments_on_photo_id   (photo_id)
+#  index_likes_on_fan_id    (fan_id)
+#  index_likes_on_photo_id  (photo_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (author_id => users.id)
+#  fk_rails_...  (fan_id => users.id)
 #  fk_rails_...  (photo_id => photos.id)
 #
 require "test_helper"
 
-class CommentTest < ActiveSupport::TestCase
+class LikeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
