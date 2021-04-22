e451b7b4c727774231695e9bec4af2e741602640
ericshagrin <ericshagrin@uchicago.edu>
2021-04-22 19:27:20 +0000

    Finished

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,14 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
-  get "/:username" => "users#show"
+
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
+
+
+  get "/:username" => "users#show",  as: :user
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
